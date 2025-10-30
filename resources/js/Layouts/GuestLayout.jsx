470d722d342c2082b43ce4a9e666fa3679d81f07
Muhamad Gia Mardhotillah <gia95194@gmail.com>
2025-10-30 20:38:54 +0700

    Redign shadow

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -5,7 +5,7 @@ export default function Guest({ children }) {
     return (
         <div className="h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
 
-            <div className="mt-6  bg-white shadow-md overflow-hidden sm:rounded-lg">
+            <div className="mt-6 drop-shadow-[0_4px_12px_rgba(0,0,0,0.25)] bg-white overflow-hidden sm:rounded-lg ">
                 {children}
             </div>
         </div>
