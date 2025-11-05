9677f47e8d8a1337b0405d40a963b43d09656af9
Muhamad Gia Mardhotillah <gia95194@gmail.com>
2025-11-05 14:59:51 +0700

    Add product controller

---
+++ database/migrations/2025_11_05_015727_create_products_table.php
@@ -0,0 +1,31 @@
+<?php
+
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('prd_code')->unique();
+            $table->string('prd_name');
+            $table->decimal('prd_price', 12, 2)->nullable();
+            $table->integer('prd_stock')->nullable();
+            $table->text('prd_description')->nullable();
+            $table->string('prd_img')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
