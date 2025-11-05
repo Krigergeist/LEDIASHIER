f2b60864e159fe90ab7192d677f8b9acc0ee1716
Muhamad Gia Mardhotillah <gia95194@gmail.com>
2025-11-06 00:53:10 +0700

    Fix the navbar to be fixed potition and add product button

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -4,122 +4,93 @@ import Dropdown from '@/Components/Dropdown';
 import NavLink from '@/Components/NavLink';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
 import { Link } from '@inertiajs/react';
+import {
+    Typography,
+    List,
+    ListItem,
+    ListItemPrefix,
+} from "@material-tailwind/react";
+import {
+    PresentationChartBarIcon,
+} from "@heroicons/react/24/solid";
 
 export default function Authenticated({ user, header, children }) {
     const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(false);
 
     return (
-        <div className="min-h-screen bg-gray-100">
-            <nav className="bg-white border-b border-gray-100">
-                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                    <div className="flex justify-between h-16">
-                        <div className="flex">
-                            <div className="shrink-0 flex items-center">
-                                <Link href="/">
-                                    <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800" />
-                                </Link>
+        <div className="flex w-full bg-gray-100 overflow-x-hidden">
+            <div className="fixed top-0 left-0 h-screen min-w-[20rem] bg-sky-500 shadow-[4px_0px_12px_rgba(0,0,0,0.12)] rounded-r-xl flex flex-col justify-between">
+                <Link href={"/"}>
+                    <div className="mb-2 p-[34px] hover:bg-sky-400 transition border-b-[3px] border-white rounded-tr-xl">
+                        <Typography variant="h5" color="blue-gray">
+                            <div className="bg-white inline-block text-transparent bg-clip-text font-bold text-4xl font-sans">
+                                Lediashier
                             </div>
-
-                            <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                                <NavLink href={route('dashboard')} active={route().current('dashboard')}>
-                                    Dashboard
-                                </NavLink>
-                            </div>
-                        </div>
-
-                        <div className="hidden sm:flex sm:items-center sm:ms-6">
-                            <div className="ms-3 relative">
-                                <Dropdown>
-                                    <Dropdown.Trigger>
-                                        <span className="inline-flex rounded-md">
-                                            <button
-                                                type="button"
-                                                className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
-                                            >
-                                                {user.name}
-
-                                                <svg
-                                                    className="ms-2 -me-0.5 h-4 w-4"
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    viewBox="0 0 20 20"
-                                                    fill="currentColor"
-                                                >
-                                                    <path
-                                                        fillRule="evenodd"
-                                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                        clipRule="evenodd"
-                                                    />
-                                                </svg>
-                                            </button>
-                                        </span>
-                                    </Dropdown.Trigger>
-
-                                    <Dropdown.Content>
-                                        <Dropdown.Link href={route('profile.edit')}>Profile</Dropdown.Link>
-                                        <Dropdown.Link href={route('logout')} method="post" as="button">
-                                            Log Out
-                                        </Dropdown.Link>
-                                    </Dropdown.Content>
-                                </Dropdown>
-                            </div>
-                        </div>
-
-                        <div className="-me-2 flex items-center sm:hidden">
-                            <button
-                                onClick={() => setShowingNavigationDropdown((previousState) => !previousState)}
-                                className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
-                            >
-                                <svg className="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                                    <path
-                                        className={!showingNavigationDropdown ? 'inline-flex' : 'hidden'}
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth="2"
-                                        d="M4 6h16M4 12h16M4 18h16"
-                                    />
-                                    <path
-                                        className={showingNavigationDropdown ? 'inline-flex' : 'hidden'}
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth="2"
-                                        d="M6 18L18 6M6 6l12 12"
-                                    />
-                                </svg>
-                            </button>
-                        </div>
+                        </Typography>
                     </div>
-                </div>
+                </Link>
 
-                <div className={(showingNavigationDropdown ? 'block' : 'hidden') + ' sm:hidden'}>
-                    <div className="pt-2 pb-3 space-y-1">
-                        <ResponsiveNavLink href={route('dashboard')} active={route().current('dashboard')}>
-                            Dashboard
-                        </ResponsiveNavLink>
+                <List className="flex flex-col justify-between h-full p-[40px]">
+                    <div className="flex flex-col">
+                        <Link href={route('dashboard')}>
+                            <ListItem className="hover:bg-sky-400 transition hover:shadow-[0_4px_12px_rgba(0,0,0,0.12)]">
+                                <ListItemPrefix>
+                                    <PresentationChartBarIcon className="h-7 w-7 me-4 text-white " />
+                                </ListItemPrefix>
+                                <p className="text-2xl text-white font-medium">Dashboard</p>
+                            </ListItem>
+                        </Link>
+
+                        <Link href={route('products.index')}>
+                            <ListItem className="hover:bg-sky-400 transition hover:shadow-[0_4px_12px_rgba(0,0,0,0.12)]">
+                                <ListItemPrefix>
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-7 me-4 text-white">
+                                        <path
+                                            fillRule="evenodd"
+                                            d="M7.5 6v.75H5.513c-.96 0-1.764.724-1.865 1.679l-1.263 12A1.875 1.875 0 0 0 4.25 22.5h15.5a1.875 1.875 0 0 0 1.865-2.071l-1.263-12a1.875 1.875 0 0 0-1.865-1.679H16.5V6a4.5 4.5 0 1 0-9 0ZM12 3a3 3 0 0 0-3 3v.75h6V6a3 3 0 0 0-3-3Zm-3 8.25a3 3 0 1 0 6 0v-.75a.75.75 0 0 1 1.5 0v.75a4.5 4.5 0 1 1-9 0v-.75a.75.75 0 0 1 1.5 0v.75Z"
+                                            clipRule="evenodd"
+                                        />
+                                    </svg>
+                                </ListItemPrefix>
+                                <p className="text-2xl text-white font-medium">Products</p>
+                            </ListItem>
+                        </Link>
                     </div>
 
-                    <div className="pt-4 pb-1 border-t border-gray-200">
-                        <div className="px-4">
-                            <div className="font-medium text-base text-gray-800">{user.name}</div>
-                            <div className="font-medium text-sm text-gray-500">{user.email}</div>
+                    <Link
+                        method="post"
+                        href={route('logout')}
+                        as="button"
+                        className="hover:bg-sky-400 rounded-xl hover:shadow-[0_4px_12px_rgba(0,0,0,0.12)]"
+                    >
+                        <ListItem className="hover:bg-sky-400 transition hover:">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-7 text-white font-bold">
+                                <path
+                                    fillRule="evenodd"
+                                    d="M16.5 3.75a1.5 1.5 0 0 1 1.5 1.5v13.5a1.5 1.5 0 0 1-1.5 1.5h-6a1.5 1.5 0 0 1-1.5-1.5V15a.75.75 0 0 0-1.5 0v3.75a3 3 0 0 0 3 3h6a3 3 0 0 0 3-3V5.25a3 3 0 0 0-3-3h-6a3 3 0 0 0-3 3V9A.75.75 0 1 0 9 9V5.25a1.5 1.5 0 0 1 1.5-1.5h6ZM5.78 8.47a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 0 0 0 1.06l3 3a.75.75 0 0 0 1.06-1.06l-1.72-1.72H15a.75.75 0 0 0 0-1.5H4.06l1.72-1.72a.75.75 0 0 0 0-1.06Z"
+                                    clipRule="evenodd"
+                                />
+                            </svg>
+                            <p className="text-2xl text-white font-medium">Log Out</p>
+                        </ListItem>
+                    </Link>
+                </List>
+            </div>
+
+            <div className="flex flex-col flex-1 min-h-screen ml-[20rem] overflow-y-auto overflow-x-hidden">
+                {header && (
+                    <header className="flex items-center justify-between bg-white w-full shadow py-6 px-4 sm:px-6 lg:px-8 sticky top-0 z-10">
+                        <div className="w-full font-bold text-2xl">{header}</div>
+                        <div className="w-fit">
+                            <p className="whitespace-nowrap text-lg">
+                                Hello, <b>{user.usr_name}</b>
+                            </p>
                         </div>
+                    </header>
+                )}
 
-                        <div className="mt-3 space-y-1">
-                            <ResponsiveNavLink href={route('profile.edit')}>Profile</ResponsiveNavLink>
-                            <ResponsiveNavLink method="post" href={route('logout')} as="button">
-                                Log Out
-                            </ResponsiveNavLink>
-                        </div>
-                    </div>
-                </div>
-            </nav>
-
-            {header && (
-                <header className="bg-white shadow">
-                    <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">{header}</div>
-                </header>
-            )}
-
-            <main>{children}</main>
+                <main className="p-4 flex flex-col ">{children}</main>
+            </div>
         </div>
     );
 }
