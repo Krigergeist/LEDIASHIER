f1a7bf5662d2ba6e0e035d37e32505544e66a757
Muhamad Gia Mardhotillah <gia95194@gmail.com>
2025-11-06 00:53:48 +0700

    Add product route

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\SupportController;
-
+use App\Http\Controllers\ProductController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -45,3 +45,10 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__ . '/auth.php';
+
+
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::resource('products', ProductController::class);
+});
