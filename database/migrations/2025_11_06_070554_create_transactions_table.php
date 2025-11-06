0b19bd440db3ea3c193c80ad9fa538ee23311fe6
Muhamad Gia Mardhotillah <gia95194@gmail.com>
2025-11-06 14:09:57 +0700

    Create transaction migration

---
+++ database/migrations/2025_11_06_070554_create_transactions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class CreateTransactionsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->bigIncrements('tsn_id');
+            $table->unsignedBigInteger('tsn_usr_id');
+            $table->unsignedBigInteger('tsn_csm_id')->nullable();
+            $table->dateTime('tsn_date')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->string('tsn_metode')->nullable();
+            $table->decimal('tsn_total', 15, 2)->default(0);
+            $table->decimal('tsn_paid', 15, 2)->default(0);
+            $table->decimal('tsn_paid_return', 15, 2)->default(0);
+            $table->timestamps();
+
+            $table->foreign('tsn_usr_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('tsn_csm_id')->references('csm_id')->on('customers')->onDelete('set null');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
