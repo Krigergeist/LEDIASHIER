a0e9db635a5f7a71c001368ce13c2f30ba73f027
Muhamad Gia Mardhotillah <gia95194@gmail.com>
2025-11-06 16:22:48 +0700

    Modify product factory data

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -1,27 +1,19 @@
 <?php
 
-
 namespace Database\Factories;
 
-
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
-
 
 class ProductFactory extends Factory
 {
-    protected $model = \App\Models\Product::class;
-
-
     public function definition()
     {
-        $name = $this->faker->words(3, true);
         return [
-            'prd_code' => Str::upper(\Illuminate\Support\Str::random(6)),
-            'prd_name' => ucfirst($name),
-            'prd_price' => $this->faker->randomFloat(2, 5, 500),
-            'prd_stock' => $this->faker->numberBetween(0, 200),
-            'prd_description' => $this->faker->sentence(12),
+            'prd_code' => strtoupper($this->faker->bothify('PRD###')),
+            'prd_name' => $this->faker->word(),
+            'prd_price' => $this->faker->numberBetween(10000, 500000),
+            'prd_stock' => $this->faker->numberBetween(1, 100),
+            'prd_description' => $this->faker->sentence(),
             'prd_img' => null,
         ];
     }
