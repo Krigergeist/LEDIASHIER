a08d9c2bed6416f72050a85891595c09091a4b85
Muhamad Gia Mardhotillah <gia95194@gmail.com>
2025-11-07 08:49:44 +0700

    Add transaction navigation

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -55,6 +55,17 @@ export default function Authenticated({ user, header, children }) {
                                 <p className="text-2xl text-white font-medium">Products</p>
                             </ListItem>
                         </Link>
+                        <Link href={route('transactions.index')}>
+                            <ListItem className="hover:bg-sky-400 transition hover:shadow-[0_4px_12px_rgba(0,0,0,0.12)]">
+                                <ListItemPrefix>
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-7 me-4 text-white">
+                                        <path d="M2.25 2.25a.75.75 0 0 0 0 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 0 0-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 0 0 0-1.5H5.378A2.25 2.25 0 0 1 7.5 15h11.218a.75.75 0 0 0 .674-.421 60.358 60.358 0 0 0 2.96-7.228.75.75 0 0 0-.525-.965A60.864 60.864 0 0 0 5.68 4.509l-.232-.867A1.875 1.875 0 0 0 3.636 2.25H2.25ZM3.75 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM16.5 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z" />
+                                    </svg>
+
+                                </ListItemPrefix>
+                                <p className="text-2xl text-white font-medium">Transaction</p>
+                            </ListItem>
+                        </Link>
                     </div>
 
                     <Link
