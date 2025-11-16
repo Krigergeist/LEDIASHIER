f4940380bb7d7b0e0c3a800b3cb384147eca95f4
Muhamad Gia Mardhotillah <gia95194@gmail.com>
2025-11-16 21:39:26 +0700

    Modify product card appearance

--- resources/js/Components/ProductCard.jsx
+++ resources/js/Components/ProductCard.jsx
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
 import { router } from "@inertiajs/react";
 
-export default function ProductCard({ product }) {
+export default function ProductCard({ product, index }) {
+    const alternateStyle =
+        index % 2 === 0
+            ? "shadow-[0_4px_12px_rgba(0,0,0,0.15)] bg-white"
+            : "shadow-[0_6px_16px_rgba(0,0,0,0.20)] bg-sky-50";
+
     const [showDesc, setShowDesc] = useState(false);
 
     const handleDelete = () => {
@@ -16,17 +21,24 @@ export default function ProductCard({ product }) {
 
 
     return (
-        <div className="bg-white rounded-lg shadow p-5 w-full max-w-[260px] flex flex-col items-center text-center shadow-[0_4px_12px_rgba(0,0,0,0.12)]">
-            <div className="w-full h-[160px] flex flex-col items-center justify-center bg-[#E8F0FE] rounded-md mb-4 overflow-hidden shadow-[inset_0_4px_12px_rgba(0,0,0,0.12)]">
+        <div className="bg-white rounded-lg shadow p-5 w-full max-w-[260px] flex flex-col items-center text-center shadow-[0_4px_12px_rgba(0,0,0,0.12)] ">
+            <div className="w-full h-[160px] relative bg-white rounded-md mb-4 overflow-hidden shadow-md flex items-center justify-center">
+
                 {product.prd_img ? (
                     <img
                         src={`/storage/${product.prd_img}`}
                         alt={product.prd_name}
-                        className="max-h-full max-w-full object-cover"
+                        className="w-full h-full object-cover"
                     />
                 ) : (
-                    <div className="text-gray-400">No Image</div>
+                    <span className="text-gray-400 font-medium absolute shadow-[inset_0_4px_12px_rgba(0,0,0,0.06)] flex items-center justify-center">
+                        No Image
+                    </span>
                 )}
+
+                {/* Inner Shadow */}
+                <div className="absolute inset-0 pointer-events-none shadow-[inset_0_8px_20px_rgba(0,0,0,0.25)]"></div>
+
             </div>
 
             <div className="flex flex-col justify-end w-full text-left">
