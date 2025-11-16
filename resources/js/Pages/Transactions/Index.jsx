3a1f4aca2a919e5568d78f553fe0b3c416cd6b18
Muhamad Gia Mardhotillah <gia95194@gmail.com>
2025-11-16 21:45:47 +0700

    Fix trasaction index view

--- resources/js/Pages/Transactions/Index.jsx
+++ resources/js/Pages/Transactions/Index.jsx
@@ -27,29 +27,29 @@ export default function Index() {
             <Head title="Transactions" />
 
             <div className="p-6 flex flex-col">
-                <div className="flex flex-col bg-white p-[20px] rounded-2xl shadow-[0_4px_12px_rgba(0,0,0,0.12)]">
-                    <div className="flex justify-between mb-4 items-center">
-                        <h1 className="text-xl font-bold align-middle">Daftar Transaksi</h1>
+                <div className="flex flex-col space-y-6 bg-white p-6 rounded-2xl shadow-[0_4px_12px_rgba(0,0,0,0.12)] ">
+                    <div className="flex justify-between  items-center">
+                        <h1 className="text-xl font-bold">Daftar Transaksi</h1>
                         <Link
                             href="/transactions/create"
-                            className="bg-sky-500 hover:bg-sky-600 text-white px-4 py-2 rounded"
+                            className="bg-sky-500 hover:bg-sky-600 text-white px-4 py-2 rounded-lg shadow-[0_4px_12px_rgba(0,0,0,0.12)] transition"
                         >
                             Buat Transaksi
                         </Link>
                     </div>
 
-                    <form onSubmit={handleSearch} className="flex w-1/2 mb-4">
+                    <form onSubmit={handleSearch} className="flex w-1/2 ">
                         <input
                             type="text"
                             value={search}
                             onChange={(e) => setSearch(e.target.value)}
                             placeholder="Cari customer..."
-                            className="w-full border-none placeholder-[#6F6F6F] shadow-[inset_0_4px_12px_rgba(0,0,0,0.12)] px-3 py-2 rounded-l-md"
+                            className="w-full border-none placeholder-[#6F6F6F] shadow-[inset_0_4px_12px_rgba(0,0,0,0.12)]  hover:border-blue-500 px-3 py-2 rounded-l-md"
                         />
-                        <button className="px-4 bg-green-500 text-white rounded">Cari</button>
+                        <button className="bg-green-500 hover:bg-green-600 text-white px-4 rounded-r-lg shadow-[0_4px_12px_rgba(0,0,0,0.12)] transition">Cari</button>
                     </form>
 
-                    <div className='bg-[#d9d9d9] h-[5px] w-full mb-6 rounded-md'>.</div>
+                    <div className='bg-[#d9d9d9] h-[5px] w-full mb-6 rounded-md'></div>
 
                     {flash?.success && (
                         <div className="mb-4 p-3 bg-green-100 text-green-700 rounded">{flash.success}</div>
@@ -103,12 +103,12 @@ export default function Index() {
                                             Rp {parseInt(tx.tsn_total).toLocaleString("id-ID")}
                                         </td>
                                         <td className="p-2 border flex gap-2">
-                                            <Link
+                                            {/* <Link
                                                 href={`/transactions/${tx.tsn_id}/edit`}
                                                 className="px-2 py-1 bg-yellow-500 text-white rounded text-center"
                                             >
                                                 Ubah
-                                            </Link>
+                                            </Link> */}
                                             <button
                                                 onClick={() => {
                                                     if (confirm("Yakin hapus transaksi ini?")) {
@@ -138,7 +138,7 @@ export default function Index() {
                                 key={i}
                                 disabled={!link.url}
                                 onClick={() => link.url && router.visit(link.url)}
-                                className={`px-3 py-1 rounded shadow-[0_4px_12px_rgba(0,0,0,0.12)]  ${link.active ? "bg-blue-500 text-white" : "bg-white"
+                                className={`px-3 py-1 rounded-lg text-sm shadow-[0_4px_12px_rgba(0,0,0,0.12)] border  ${link.active ? "bg-sky-500 text-white border-sky-500" : "bg-white text-gray-700 hover:bg-gray-100"
                                     }`}
                                 dangerouslySetInnerHTML={{ __html: link.label }}
                             />
