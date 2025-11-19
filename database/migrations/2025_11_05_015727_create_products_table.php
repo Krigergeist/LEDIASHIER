86bcac8493f9f312a0d227dbd8c9703b5db021cc
Muhamad Gia Mardhotillah <gia95194@gmail.com>
2025-11-19 12:49:02 +0700

    Remove prd_code

--- database/migrations/2025_11_05_015727_create_products_table.php
+++ database/migrations/2025_11_05_015727_create_products_table.php
@@ -12,7 +12,6 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('prd_id');
-            $table->string('prd_code')->unique();
             $table->string('prd_name');
             $table->decimal('prd_price', 12, 2)->nullable();
             $table->integer('prd_stock')->nullable();
